0026da00016c4745d7e8e259e88349cb491fc771
sankarsai505 <120040682+sankarsai505@users.noreply.github.com>
2023-02-18 16:42:11 +0530

    Add files via upload

---
+++ mul.js
@@ -0,0 +1,6 @@
+const num1=Math.ceil(Math.random()*10)
+const num2=Math.ceil(Math.random()*10)
+const question1=document.getElementById("question");
+question1.innerText=`what is ${num1} mul by ${num2}`
+
+
